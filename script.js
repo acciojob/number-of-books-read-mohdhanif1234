fcaad47799630e817f4a6737565df2d4de38e82d
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2023-06-22 23:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let n = library.length;
+	let count = 0;
+	for(let i = 0; i < n; i++){
+		if(library[i].readingStatus){
+			count++;
+		}
+	}
+	return count;
 };
 
 // Do not change the code below
